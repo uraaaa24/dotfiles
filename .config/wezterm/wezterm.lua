33da8745362b54f7f72a7c0713a228b2b643f4ce
Yuto Urashima <yuto.urashima24@gmail.com>
2026-01-30 21:37:59 +0900

    feat: adjust wezterm window background opacity and enhance tab title formatting

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,26 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.85
 config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
+
+ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+   local background = "#5c6d74"
+   local foreground = "#FFFFFF"
+
+   if tab.is_active then
+     background = "#ae8b2d"
+     foreground = "#FFFFFF"
+   end
+
+   local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+   return {
+     { Background = { Color = background } },
+     { Foreground = { Color = foreground } },
+     { Text = title },
+   }
+ end)
 
 return config
