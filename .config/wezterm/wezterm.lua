425bf150ee11d240d53077df74d21a4514635678
Yuto Urashima <yuto.urashima24@gmail.com>
2026-01-30 21:27:19 +0900

    feat: update wezterm configuration for background opacity and blur settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,5 +4,7 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 20
 
 return config
