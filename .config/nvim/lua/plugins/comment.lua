bed5c7973a8c93b610d3dfe47d79ba2d2f72ba87
Yuto Urashima <yuto.urashima24@gmail.com>
2026-07-13 08:49:43 +0900

    chore: add comment feature in nvim

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,6 +1,22 @@
 return {
   {
     "numToStr/Comment.nvim",
+    keys = {
+      {
+        "<leader>/",
+        function()
+          require("Comment.api").toggle.linewise.current()
+        end,
+        desc = "Toggle comment",
+        mode = "n",
+      },
+      {
+        "<leader>/",
+        '<esc><cmd>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<cr>',
+        desc = "Toggle comment",
+        mode = "v",
+      },
+    },
     opts = {},
   },
 }
